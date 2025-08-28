c61b42297bfa41d01207b6456072c76f8eecf81f
SixtenE <sixten.ekblad@gmail.com>
2025-08-28 14:35:20 +0200

    add html page

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import mongoose from 'mongoose'
 import ComposerModel from './models/composerModel'
 import composerRouter from './routes/composerRoutes'
 import seedRouter from './routes/seedRoutes'
+import rateLimit from 'express-rate-limit'
+import path from 'path'
 
 mongoose.connect(String(process.env.DATABASE_URL), {
     dbName: 'workshop-composers',
@@ -14,8 +16,22 @@ const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000
 const app = express()
 app.use(express.json())
 
+app.use(
+    rateLimit({
+        windowMs: process.env.RATE_LIMIT_WINDOW_MS
+            ? parseInt(process.env.RATE_LIMIT_WINDOW_MS) * 1000
+            : 15 * 60 * 1000 * 100,
+        max: process.env.RATE_LIMIT_MAX
+            ? parseInt(process.env.RATE_LIMIT_MAX)
+            : 50,
+        message: 'Too many requests from this IP, please try again later.',
+    }),
+)
+
 app.use('/api/composers', composerRouter)
 app.use('/api/seed', seedRouter)
+
+app.use('/', express.static('public'))
 
 const server = app.listen(PORT, '::', () => {
     console.log(`Server is running at ${JSON.stringify(server.address())}`)
